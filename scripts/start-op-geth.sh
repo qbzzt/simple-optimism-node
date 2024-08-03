af17e55cd7f6cd0cdb699d308716bb8c80bc19ef
Ori Pomerantz <qbzzt1@gmail.com>
2024-08-02 21:56:23 -0500

    WIP

--- scripts/start-op-geth.sh
+++ scripts/start-op-geth.sh
@@ -29,35 +29,41 @@ if [ -z "${OP_GETH__SYNCMODE+x}" ]; then
   fi
 fi
 
-# Start op-geth.
 exec geth \
   --datadir="$BEDROCK_DATADIR" \
+  --syncmode="$OP_GETH__SYNCMODE" \
+  --gcmode="$NODE_TYPE" \
+  --networkid 690 \
+  --ipcdisable \
   --http \
+  --http.port=8545 \
+  --http.api="admin,engine,eth,web3,txpool,net,debug,net" \
+  --http.addr="0.0.0.0" \
   --http.corsdomain="*" \
   --http.vhosts="*" \
-  --http.addr=0.0.0.0 \
-  --http.port=8545 \
-  --http.api=web3,debug,eth,txpool,net,engine \
   --ws \
-  --ws.addr=0.0.0.0 \
+  --ws.api="debug,eth,txpool,net,web3,engine" \
   --ws.port=8546 \
+  --ws.addr="0.0.0.0" \
   --ws.origins="*" \
-  --ws.api=debug,eth,txpool,net,engine,web3 \
   --metrics \
   --metrics.influxdb \
   --metrics.influxdb.endpoint=http://influxdb:8086 \
   --metrics.influxdb.database=opgeth \
-  --syncmode="$OP_GETH__SYNCMODE" \
-  --gcmode="$NODE_TYPE" \
+  --networkid=690 \
+  --verbosity=3 \
   --authrpc.vhosts="*" \
   --authrpc.addr=0.0.0.0 \
   --authrpc.port=8551 \
   --authrpc.jwtsecret=/shared/jwt.txt \
+  --rpc.allow-unprotected-txs \
+  --gpo.minsuggestedpriorityfee=1000000 \
   --rollup.sequencerhttp="$BEDROCK_SEQUENCER_HTTP" \
   --rollup.disabletxpoolgossip=true \
   --port="${PORT__OP_GETH_P2P:-39393}" \
   --discovery.port="${PORT__OP_GETH_P2P:-39393}" \
   --db.engine=pebble \
+  --nodiscover \
   --state.scheme=hash \
   $EXTENDED_ARG $@
 
--- scripts/start-op-node.sh
+++ scripts/start-op-node.sh
@@ -14,17 +14,30 @@ else
 fi
 
 # Start op-node.
+
+
+
+
 exec op-node \
   --l1=$OP_NODE__RPC_ENDPOINT \
+  --l1.beacon.ignore \
   --l2=http://op-geth:8551 \
-  --rpc.addr=0.0.0.0 \
-  --rpc.port=9545 \
   --l2.jwt-secret=/shared/jwt.txt \
+  --plasma.enabled \
+  --plasma.da-server=https://da.redstonechain.com \
   --l1.trustrpc \
   --l1.rpckind=$OP_NODE__RPC_TYPE \
-  --l1.beacon=$OP_NODE__L1_BEACON \
   --metrics.enabled \
   --metrics.addr=0.0.0.0 \
   --metrics.port=7300 \
+  --rpc.addr=0.0.0.0 \
+  --rpc.port=9545 \
+  --rpc.enable-admin \
+  --p2p.bootnodes=enr:-J24QIpQjoWf3sSMiDk3CGkA3FaUVadLzMxuJHfEvkE5Q9FbFRFG5RPFxCIw0b7boSCjJ_vVc8pX4Ue-tVjsu4ou7-qGAY8vobQSgmlkgnY0gmlwhDTWuneHb3BzdGFja4OyBQCJc2VjcDI1NmsxoQMTlWaih1oq9kVe5yYJ5N1C0IAqx4mpxJyl8L17-y4csYN0Y3CCJAaDdWRwgiQG,enr:-J24QF7mHVOC1BiVPXWl2IC_FUbSHeuci1NyCd78M7465KOIWeSwViFJePXBManyOebiwk-SA5hESbORtXKWYtw73WOGAY8vnYXtgmlkgnY0gmlwhDaqpwSHb3BzdGFja4OyBQCJc2VjcDI1NmsxoQLNE3bfX7m_0n1lEitv4YKTTsTvZr0jmTXJS2x6rGSI9IN0Y3CCJAaDdWRwgiQG,enr:-J24QPBqZvUSnkzKgjnkYB_VvLaFW3xTMvOZeYIX1d5PAnKYIeIGCU206MOdVs_WyYmSUvaPIwXmRvyaKZWlJJYh0KGGAY8vlbMHgmlkgnY0gmlwhCLwOnqHb3BzdGFja4OyBQCJc2VjcDI1NmsxoQIW0taIKXJ_N2bUMmUL2QQzj6l9rk9sR_9YaWY2lazOwoN0Y3CCJAaDdWRwgiQG \
+  --rollup.config=/chainconfig/rollup.json   \
   --syncmode=execution-layer \
   $EXTENDED_ARG $@
+
+
+
+
